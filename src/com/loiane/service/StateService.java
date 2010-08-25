7e5892e30fb5ae1196968cf844d13c09ab020799
loiane <loiane@loianeg.br.ibm.com>
2010-08-25 13:54:57 -0300

    added transactions

--- src/com/loiane/service/StateService.java
+++ src/com/loiane/service/StateService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.loiane.dao.StateDAO;
 import com.loiane.model.State;
@@ -24,6 +25,7 @@ public class StateService {
 	 * Return list of all States of Brazil
 	 * @return
 	 */
+	@Transactional(readOnly=true)
 	public List<State> getBrazilianStates(){
 		
 		return stateDAO.getStates();
